737082d51dc805e1cd7b537c52f22fa53d2b4db1
YIFEI Ming <alvinming@instgpu-01.cs.wisc.edu>
2022-02-27 17:22:10 -0600

    add save_csv func for evaluation results

--- eval_ood_detection.py
+++ eval_ood_detection.py
@@ -16,7 +16,7 @@ def process_args():
     parser.add_argument('--in_dataset', default="ImageNet10", type=str, choices = ['CIFAR-10', 'CIFAR-100', 'ImageNet', 'ImageNet10'], help='in-distribution dataset')
     parser.add_argument('--gpus', default=[4], nargs='*', type=int,
                             help='List of GPU indices to use, e.g., --gpus 0 1 2 3')
-    parser.add_argument('-b', '--batch-size', default=400, type=int,
+    parser.add_argument('-b', '--batch-size', default=200, type=int,
                             help='mini-batch size')
     parser.add_argument('--model', default='CLIP', type=str, help='model architecture')
     parser.add_argument('--CLIP_ckpt', type=str, default='ViT-B/16',
@@ -85,11 +85,85 @@ def save_as_dataframe(args, out_datasets, fpr_list, auroc_list, aupr_list):
     aupr_list = [float('{:.2f}'.format(100*aupr)) for aupr in aupr_list]
     import pandas as pd
     data = {k:v for k,v in zip(out_datasets, zip(fpr_list,auroc_list,aupr_list))}
+    data['AVG'] = [np.mean(fpr_list),np.mean(auroc_list),np.mean(aupr_list) ]
+    data['AVG']  = [float('{:.2f}'.format(100*metric)) for metric in data['AVG']]
     # Specify orient='index' to create the DataFrame using dictionary keys as rows
     df = pd.DataFrame.from_dict(data, orient='index',
                        columns=['FPR95', 'AURPC', 'AUPR'])
     df.to_csv(os.path.join(args.log_directory,f'{args.name}.csv'))
+
+
+def obtain_feature_from_loader(net, loader, layer_idx, embedding_dim, num_batches):
+    out_features = torch.zeros((0, embedding_dim), device = 'cuda')
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(loader):
+            if num_batches is not None:
+                if batch_idx >= num_batches:
+                    break
+            data, target = data.cuda(), target.cuda()
+            out_feature = net.intermediate_forward(data, layer_idx) 
+            if layer_idx == 0: # out_feature: bz, 512, 4, 4
+                out_feature = out_feature.view(out_feature.size(0), out_feature.size(1), -1) #bz, 512, 16
+                out_feature = torch.mean(out_feature, 2) # bz, 512
+                out_feature =F.normalize(out_feature, dim = 1)
+            out_features = torch.cat((out_features,out_feature), dim = 0)
+    return out_features       
+
+def knn(layer_idx = 0, num_classes = 100):
+    args = process_args()
+    train_loader, test_loader = set_loader(args, eval = True)
+    # args.ckpt = 'yiyou_checkpoint_500.pth.tar'
+    pretrained_dict= torch.load(args.ckpt,  map_location='cpu')['state_dict']
+    
+    pretrained_dict = {key.replace("module.", ""): value for key, value in pretrained_dict.items()}
+    net, criterion = set_model(args)
+    net.load_state_dict(pretrained_dict)
+    net.eval()
+    ood_num_examples = len(test_loader.dataset) 
+    num_batches = ood_num_examples // args.batch_size
+    if layer_idx == 1:
+        embedding_dim = 128
+    elif layer_idx == 0:
+        embedding_dim = 512 #for resnet-18 and34
+    ftrain = obtain_feature_from_loader(net, train_loader, layer_idx, embedding_dim, num_batches = None)
+    ftest = obtain_feature_from_loader(net, test_loader, layer_idx, embedding_dim, num_batches = None)
+    print('preprocessing ID finished')
+    # out_datasets = [ 'SVHN', 'places365', 'iSUN', 'dtd', 'LSUN']
+    out_datasets = [ 'cifar10']
+    food_all = {}
+    auroc_list, aupr_list, fpr_list = [], [], []
+    for out_dataset in out_datasets:
+        ood_loader = set_ood_loader(args, out_dataset)
+        ood_feat = obtain_feature_from_loader(net, ood_loader, layer_idx, embedding_dim, num_batches)
+        food_all[out_dataset] = ood_feat
+        print(f'preprocessing OOD {out_dataset} finished')
+    index = faiss.IndexFlatL2(ftrain.shape[1])
+    index.add(ftrain.cpu().numpy())
+    index_bad = index
+    ################### Using KNN distance Directly ###################
+    K = 200
+    D, _ = index_bad.search(ftest.cpu().numpy(), K, )
+    scores_in = -D[:,-1]
+    # scores_in = -D.mean(1)
+    # all_results = []
+    for ood_dataset, food in food_all.items():
+        print(f"Evaluting OOD dataset {ood_dataset}")
+        D, _ = index_bad.search(food.cpu().numpy(), K)
+        scores_ood_test = -D[:,-1]
+        # results = metrics.cal_metric(scores_in, scores_ood_test)
+        # all_results.append(results)
+        aurocs, auprs, fprs = [], [], []
+        # out as pos 
+        # measures = get_measures(scores_ood_test, scores_in)
+        measures = get_measures(scores_in, scores_ood_test)
+        aurocs.append(measures[0]); auprs.append(measures[1]); fprs.append(measures[2])
+        print(scores_in[:3], scores_ood_test[:3])
+        auroc = np.mean(aurocs); aupr = np.mean(auprs); fpr = np.mean(fprs)
+        auroc_list.append(auroc); aupr_list.append(aupr); fpr_list.append(fpr)
+        print_measures(None, auroc, aupr, fpr, args.method_name)
         
+    print("AVG")
+    print_measures(None, np.mean(auroc_list), np.mean(aupr_list), np.mean(fpr_list), method_name=args.method_name)
 
 def main():
 
