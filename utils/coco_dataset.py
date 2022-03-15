0eaa5af3639808a074882fff58c21b1755fac109
YIFEI Ming <alvinming@instgpu-01.cs.wisc.edu>
2022-03-14 21:47:05 -0500

    correct training code

--- utils/coco_dataset.py
+++ utils/coco_dataset.py
@@ -9,16 +9,16 @@ import argparse
 from transformers import ViTFeatureExtractor, CLIPProcessor
 
 
-def build_caption_dataframe(lang, captions_dir, option = 'train'):
+def build_caption_dataframe(params, option = 'train'):
     if option == 'train': 
         image_path =os.path.join(params.image_dir, "train2014")
         captions_path = os.path.join(params.captions_dir, "processed_captions_train2014.csv")
     elif option == 'val':
         image_path =os.path.join(params.image_dir, "val2014")
         captions_path = os.path.join(params.captions_dir, "processed_captions_val2014.csv")
-    if lang == 'es':
+    if params.lang == 'es':
         df = pd.read_csv(f"{captions_path}", encoding = 'utf-8-sig')
-    elif lang == 'en':
+    elif params.lang == 'en':
         df = pd.read_csv(f"{captions_path}")
     x = list(set(df['image_id'].values))
     image_ids = np.arange(0, len(x))
@@ -31,18 +31,18 @@ def build_image_loader(params, df, image_path, option):
     image_prefix = f'COCO_{option}2014_'
     image_filenames = [f"{image_path}/{image_prefix}{str(image_ids[i]).zfill(12)}.jpg" for i in range(len(image_ids))] 
     
-    dataset = CLIPDataset_ViT(image_filenames, df["caption"].values)
+    dataset = CLIPDataset_ViT(params, image_filenames, df["caption"].values)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=params.batch_size, num_workers=params.num_workers, shuffle=(option == 'train'))
     return dataloader
 
 def build_coco_loader(params, option = 'train'):
-    df, image_path = build_caption_dataframe(params.lang, params.captions_dir, option)
+    df, image_path = build_caption_dataframe(params, option)
     coco_loader = build_image_loader(params, df, image_path, option)
     return coco_loader
 
 
 class CLIPDataset_ViT(torch.utils.data.Dataset):
-    def __init__(self, image_filenames, captions, transforms=None):
+    def __init__(self, params, image_filenames, captions):
         """
         image_filenames and cpations must have the same length; so, if there are
         multiple captions for each image, the image_filenames must have repetitive
@@ -51,7 +51,7 @@ class CLIPDataset_ViT(torch.utils.data.Dataset):
         self.captions = list(captions)
         self.image_filenames = image_filenames
         # self.feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
-        self.feature_extractor  = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.feature_extractor  = CLIPProcessor.from_pretrained(params.ckpt)
 
     def __getitem__(self, idx):
         image = cv2.imread(self.image_filenames[idx]) 
@@ -60,7 +60,7 @@ class CLIPDataset_ViT(torch.utils.data.Dataset):
         inputs = self.feature_extractor(images = img, return_tensors="pt") # transform to tensors already
 
         item = {}
-        item['image'] = inputs['pixel_values'][0]  # inputs['pixel_values'].shape: torch.Size([1, 3, 224, 224])
+        item['pixel_values'] = inputs['pixel_values'][0]  # inputs['pixel_values'].shape: torch.Size([1, 3, 224, 224])
         item['caption'] = self.captions[idx]
         return item
 
@@ -73,13 +73,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test clip loader')
     parser.add_argument("--lang", type=str, default='en', help="Source language")
     parser.add_argument("--dataset", type=str, default='COCO', help="Source language")
-    parser.add_argument("--caption_dir", type=str, default='data', help="Source language")
+    parser.add_argument("--root_dir", type=str, default='data', help="Source language")
 
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--num_workers", type=int, default=4)
     params = parser.parse_args()
 
     params.image_dir = f'/nobackup/COCO/COCO-14'
-    params.captions_dir = f"{params.caption_dir}/{params.dataset}/captions/{params.lang}"
+    params.captions_dir = f"{params.root_dir}/{params.dataset}/captions/{params.lang}"
     train_loader = build_coco_loader(params, option = 'train')
     val_loader = build_coco_loader(params, option = 'val')
