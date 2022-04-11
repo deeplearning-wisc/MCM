8ebcf057a74a2d3a7fa182c3fd8c4fa880d2dda2
Yifei Ming <ming5@wisc.edu>
2022-04-11 14:46:44 -0500

    nouns

--- utils/plot_util.py
+++ utils/plot_util.py
@@ -11,7 +11,7 @@ import seaborn as sns
 import torch.nn.functional as F
 # plot kde plots
 def plot_distribution(args, id_scores, ood_scores, out_dataset):
-    args.score = 'CLS'
+    # args.score = 'CLS'
     sns.set(style="white", palette="muted")
     sns.displot({"ID":-1 * id_scores, "OOD": -1 * ood_scores}, label="id", kind = "kde", fill = True, alpha = 0.5)
     plt.title(f"ID v.s. {out_dataset} {args.score} score")
