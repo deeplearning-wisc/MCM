e679e2d471ca2941d8ed00fe7efdb3f3ff0e5d93
Yifei Ming <ming5@wisc.edu>
2022-12-04 23:44:50 -0600

    Update eval_mcm.sh

--- scripts/eval_mcm.sh
+++ scripts/eval_mcm.sh
@@ -5,4 +5,4 @@ SCORE=$3
 CKPT=ViT-B/16
 DATA_ROOT=datasets
 
-python eval_ood_detection.py --in_dataset $ID --name $EXP_NAME --CLIP_ckpt $CKPT --score $SCORE --root-dir $DATA_ROOT
+python eval_ood_detection.py --in_dataset ${ID} --name ${EXP_NAME} --CLIP_ckpt ${CKPT} --score ${SCORE} --root-dir ${DATA_ROOT}
