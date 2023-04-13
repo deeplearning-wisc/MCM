c98f2d2772a97aaa8caff0a55265761146338383
Yifei Ming <ming5@wisc.edu>
2023-04-13 00:12:51 -0500

    update classname mismatch

--- utils/common.py
+++ utils/common.py
@@ -49,8 +49,8 @@ def obtain_ImageNet10_classes():
 def obtain_ImageNet20_classes():
 
     class_dict = {"n04147183": "sailboat", "n02951358": "canoe", "n02782093": "balloon", "n04389033": "tank", "n03773504": "missile",
-                  "n02917067": "bullet train", "n02317335": "starfish", "n01632458": "spotted salamander", "n01630670": "common newt", "n01631663": "zebra",
-                  "n02391049": "frilled lizard", "n01693334": "green lizard", "n01697457": "African crocodile", "n02120079": "Arctic fox", "n02114367": "timber wolf",
+                  "n02917067": "bullet train", "n02317335": "starfish", "n01632458": "spotted salamander", "n01630670": "common newt", "n01631663": "eft",
+                  "n02391049": "zebra", "n01693334": "green lizard", "n01697457": "African crocodile", "n02120079": "Arctic fox", "n02114367": "timber wolf",
                   "n02132136": "brown bear", "n03785016": "moped", "n04310018": "steam locomotive", "n04266014": "space shuttle", "n04252077": "snowmobile"}
     # sort by values
     class_dict = {k: v for k, v in sorted(
